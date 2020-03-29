57a1bd6d4c1d450f6053a73df63183156fa596a5
Mihailo Zivkovic <jevgenije@hotmail.com>
2020-03-29 17:15:06 +0200

    JEboMater

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -14,9 +14,11 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
 import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
+import {UserService} from './_services/user.service';
+import { MemberCardComponent } from './members/member-list/member-card/member-card.component';
 
 
 
@@ -29,7 +31,8 @@ import { appRoutes } from './routes';
       RegisterComponent,
       ListsComponent,
       MemberListComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent
    ],
    imports: [
       BrowserModule,
